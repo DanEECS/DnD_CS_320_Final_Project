053627f66dac09eab560243e0d1f3c46528e0280
Matthew Fritz <matthewfritz@Matthews-MacBook-Air.local>
2019-12-03 13:00:17 -0800

    adding CharMgmtSys

---
+++ app/DND CharMgmtSys/scripts.js
@@ -0,0 +1,14 @@
+$('.ui.dropdown')
+    .dropdown()
+;
+$('select.dropdown')
+  .dropdown()
+;
+$('.category.example .ui.dropdown')
+  .dropdown({
+    allowCategorySelection: true
+  })
+;
+$('#progressBar')
+  .progress({percent:20})
+;
